c831163d5e4411e90a7f1d4ead5796528f6e11e8
mohamed <mohamed.abdirahman@edu.turkuamk.fi>
2026-04-27 17:42:06 +0300

    Made prisma migration

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+console.log("DB URL:", process.env.DATABASE_URL);
 
 app.use("/players", playerRoutes);
 app.use("/matches", matchRoutes);
